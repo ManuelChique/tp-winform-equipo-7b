c7527220d54cfd3189830f12d467773c936c9eb5
Francoaguado08 <aguadoezequiel08@gmail.com>
2024-09-06 20:00:22 -0300

    Maqueta de frmArticulos

--- TPWinForm_Equipo7B/TPWinForm/frmArticulos.Designer.cs
+++ TPWinForm_Equipo7B/TPWinForm/frmArticulos.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.chkbVerDetalle = new System.Windows.Forms.CheckBox();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.pbxArticulo = new System.Windows.Forms.PictureBox();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.btnEliminarArticulo = new System.Windows.Forms.Button();
+            this.btnModificarArticulo = new System.Windows.Forms.Button();
+            this.btnAgregarArticulo = new System.Windows.Forms.Button();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkbVerDetalle
+            // 
+            this.chkbVerDetalle.AutoSize = true;
+            this.chkbVerDetalle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkbVerDetalle.Location = new System.Drawing.Point(330, 56);
+            this.chkbVerDetalle.Margin = new System.Windows.Forms.Padding(2);
+            this.chkbVerDetalle.Name = "chkbVerDetalle";
+            this.chkbVerDetalle.Size = new System.Drawing.Size(97, 21);
+            this.chkbVerDetalle.TabIndex = 26;
+            this.chkbVerDetalle.Text = "Ver Detalle";
+            this.chkbVerDetalle.UseVisualStyleBackColor = true;
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(112, 57);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(200, 20);
+            this.txtFiltro.TabIndex = 25;
+            // 
+            // pbxArticulo
+            // 
+            this.pbxArticulo.Location = new System.Drawing.Point(561, 91);
+            this.pbxArticulo.Name = "pbxArticulo";
+            this.pbxArticulo.Size = new System.Drawing.Size(231, 244);
+            this.pbxArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxArticulo.TabIndex = 24;
+            this.pbxArticulo.TabStop = false;
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFiltro.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltro.Location = new System.Drawing.Point(8, 53);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(109, 25);
+            this.lblFiltro.TabIndex = 23;
+            this.lblFiltro.Text = "Buscar : ";
+            // 
+            // btnEliminarArticulo
+            // 
+            this.btnEliminarArticulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEliminarArticulo.Location = new System.Drawing.Point(282, 360);
+            this.btnEliminarArticulo.Name = "btnEliminarArticulo";
+            this.btnEliminarArticulo.Size = new System.Drawing.Size(111, 37);
+            this.btnEliminarArticulo.TabIndex = 22;
+            this.btnEliminarArticulo.Text = "Eliminar";
+            this.btnEliminarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarArticulo
+            // 
+            this.btnModificarArticulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnModificarArticulo.Location = new System.Drawing.Point(144, 360);
+            this.btnModificarArticulo.Name = "btnModificarArticulo";
+            this.btnModificarArticulo.Size = new System.Drawing.Size(111, 37);
+            this.btnModificarArticulo.TabIndex = 21;
+            this.btnModificarArticulo.Text = "Modificar";
+            this.btnModificarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarArticulo
+            // 
+            this.btnAgregarArticulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregarArticulo.Location = new System.Drawing.Point(8, 360);
+            this.btnAgregarArticulo.Name = "btnAgregarArticulo";
+            this.btnAgregarArticulo.Size = new System.Drawing.Size(111, 37);
+            this.btnAgregarArticulo.TabIndex = 20;
+            this.btnAgregarArticulo.Text = "Agregar";
+            this.btnAgregarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(8, 90);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.ReadOnly = true;
+            this.dgvArticulos.RowHeadersWidth = 51;
+            this.dgvArticulos.Size = new System.Drawing.Size(547, 244);
+            this.dgvArticulos.TabIndex = 19;
+            // 
+            // frmArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkbVerDetalle);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.pbxArticulo);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.btnEliminarArticulo);
+            this.Controls.Add(this.btnModificarArticulo);
+            this.Controls.Add(this.btnAgregarArticulo);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "frmArticulos";
             this.Text = "frmArticulos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox chkbVerDetalle;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.PictureBox pbxArticulo;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.Button btnEliminarArticulo;
+        private System.Windows.Forms.Button btnModificarArticulo;
+        private System.Windows.Forms.Button btnAgregarArticulo;
+        private System.Windows.Forms.DataGridView dgvArticulos;
     }
 }
